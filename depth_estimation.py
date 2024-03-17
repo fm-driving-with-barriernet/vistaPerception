0ed4743cb92c5d96df7650e322a7c83070691588
Hector Martinez <hectorxm@mit.edu>
2024-03-17 19:15:15 -0400

    initial depth_estimation commit

--- depth_estimation.py
+++ depth_estimation.py
@@ -5,5 +5,18 @@ import sys
 
 import torch
 import torch.nn.functional as F
+import numpy as np
 
 from dinov2.dinov2.eval.depth.models import build_depther
+
+"""
+Purpose of this file:
+This file will handle running inference for depth estimation on an image fed from VISTA display.render()
+It will return the depth estimation png result to later be used in downstream inputs to BarrierNet
+"""
+
+def run_depth_inference():
+    """
+    stuff here
+    """
+    return None
